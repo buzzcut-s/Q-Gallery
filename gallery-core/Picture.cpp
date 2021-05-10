0453b4818a4b3b4a6977ae0c348c55d39ee3b3b2
Piyush Kumar <piyushkumg@gmail.com>
2021-05-10 18:52:50 +0530

    Implement the Picture class

--- gallery-core/Picture.cpp
+++ gallery-core/Picture.cpp
@@ -1,1 +1,25 @@
 #include "Picture.h"
+
+#include <utility>
+
+Picture::Picture(const QString& filePath) :
+    Picture(QUrl::fromLocalFile(filePath))
+{
+}
+
+Picture::Picture(const QUrl& fileUrl) :
+    m_id(-1),
+    m_albumId(-1),
+    m_fileUrl(fileUrl)
+{
+}
+
+auto Picture::fileUrl() const -> QUrl
+{
+        return m_fileUrl;
+}
+
+void Picture::setFileUrl(const QUrl& fileUrl)
+{
+        m_fileUrl = fileUrl;
+}
--- gallery-core/Picture.h
+++ gallery-core/Picture.h
@@ -1,12 +1,30 @@
 #ifndef PICTURE_H
 #define PICTURE_H
 
+#include <QString>
+#include <QUrl>
+
 #include "gallery-core_global.h"
 
 class GALLERYCORE_EXPORT Picture
 {
 public:
-        Picture() = default;
+        Picture(const QString& filePath = "");
+        Picture(const QUrl& fileUrl);
+
+        int  id() const;
+        void setId(int id);
+
+        int  albumId() const;
+        void setAlbumId(int albumId);
+
+        QUrl fileUrl() const;
+        void setFileUrl(const QUrl& fileUrl);
+
+private:
+        int  m_id;
+        int  m_albumId;
+        QUrl m_fileUrl;
 };
 
 #endif  // PICTURE_H
