3ef4f175d42bde382cd5fe3e2633e65647c96a75
Piyush Kumar <piyushkumg@gmail.com>
2021-05-10 18:45:43 +0530

    Implement Album class

--- gallery-core/Album.cpp
+++ gallery-core/Album.cpp
@@ -1,1 +1,7 @@
 #include "Album.h"
+
+Album::Album(const QString& name) :
+    m_id(-1),
+    m_name(name)
+{
+}
--- gallery-core/Album.h
+++ gallery-core/Album.h
@@ -1,12 +1,23 @@
 #ifndef ALBUM_H
 #define ALBUM_H
 
+#include <QString>
+
 #include "gallery-core_global.h"
 
 class GALLERYCORE_EXPORT Album
 {
 public:
-        Album() = default;
+        explicit Album(const QString& name = "");
+
+        int     id() const;
+        void    setId(int id);
+        QString name() const;
+        void    setName(const QString& name);
+
+private:
+        int     m_id;
+        QString m_name;
 };
 
 #endif  // ALBUM_H
