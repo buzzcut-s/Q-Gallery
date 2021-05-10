377131df58186ea3fb14e9b2ee866d974fe59794
Piyush Kumar <piyushkumg@gmail.com>
2021-05-10 20:16:03 +0530

    Implement PictureDao class  - Similar to AlbumDao in implementation  - The main difference is that a picture has a foreign key to an album  - The PictureDao function must be conditioned by an albumId parameter  - Multiple functions take an albumId parameter to make the link between the picture and the owning album parameter  - In the init() function, the foreign key is expressed in the album_id INTEGER syntax.

--- gallery-core/PictureDao.cpp
+++ gallery-core/PictureDao.cpp
@@ -1,1 +1,61 @@
 #include "PictureDao.h"
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QVariant>
+
+#include "Picture.h"
+
+PictureDao::PictureDao(QSqlDatabase& database) :
+    m_database(database)
+{
+}
+
+void PictureDao::init() const
+{
+        if (!m_database.tables().contains("pictures"))
+        {
+                QSqlQuery query(m_database);
+
+                query.exec(QString("CREATE TABLE pictures ")
+                           + "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                           + "album_id INTEGER, "
+                           + "url TEXT)");
+        }
+}
+
+void PictureDao::addPictureInAlbum(int albumId, Picture& picture) const
+{
+        QSqlQuery query(m_database);
+
+        query.prepare(QString("INSERT INTO pictures")
+                      + " (album_id, url)"
+                      + " VALUES ("
+                      + ":album_id, "
+                      + ":url"
+                      + ")");
+        query.bindValue(":album_id", albumId);
+        query.bindValue(":url", picture.fileUrl());
+        query.exec();
+
+        picture.setId(query.lastInsertId().toInt());
+        picture.setAlbumId(albumId);
+}
+
+void PictureDao::removePicture(int id) const
+{
+        QSqlQuery query(m_database);
+
+        query.prepare("DELETE FROM pictures WHERE id = (:id)");
+        query.bindValue(":id", id);
+        query.exec();
+}
+
+void PictureDao::removePicturesForAlbum(int albumId) const
+{
+        QSqlQuery query(m_database);
+
+        query.prepare("DELETE FROM pictures WHERE album_id = (:album_id)");
+        query.bindValue(":album_id", albumId);
+        query.exec();
+}
--- gallery-core/PictureDao.h
+++ gallery-core/PictureDao.h
@@ -1,10 +1,24 @@
 #ifndef PICTUREDAO_H
 #define PICTUREDAO_H
 
+#include <QVector>
+
+class QSqlDatabase;
+class Picture;
+
 class PictureDao
 {
 public:
-        PictureDao() = default;
+        explicit PictureDao(QSqlDatabase& database);
+        void init() const;
+
+        void addPictureInAlbum(int albumId, Picture& picture) const;
+        void removePicture(int id) const;
+        void removePicturesForAlbum(int albumId) const;
+        auto picturesForAlbum(int albumId) const -> QVector<Picture*>;
+
+private:
+        QSqlDatabase& m_database;
 };
 
 #endif  // PICTUREDAO_H
