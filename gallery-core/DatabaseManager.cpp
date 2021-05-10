00711766831f4da06df5f38e5451681fb2bf3f0a
Piyush Kumar <piyushkumg@gmail.com>
2021-05-10 19:40:10 +0530

    Implement DatabaseManager class  - As a singleton  - Initialize m_database with the QSqlDatabase::addDatabase("QSQLITE") function call  - Not checking for return value of m_database->open() yet

--- gallery-core/DatabaseManager.cpp
+++ gallery-core/DatabaseManager.cpp
@@ -1,1 +1,22 @@
 #include "DatabaseManager.h"
+
+#include <QSqlDatabase>
+
+DatabaseManager& DatabaseManager::instance()
+{
+        static DatabaseManager singleton;
+        return singleton;
+}
+
+DatabaseManager::DatabaseManager(const QString& path) :
+    m_dataBase(new QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE")))
+{
+        m_dataBase->setDatabaseName(path);
+        m_dataBase->open();
+}
+
+DatabaseManager::~DatabaseManager()
+{
+        m_dataBase->close();
+        delete m_dataBase;
+}
--- gallery-core/DatabaseManager.h
+++ gallery-core/DatabaseManager.h
@@ -1,10 +1,27 @@
 #ifndef DATABASEMANAGER_H
 #define DATABASEMANAGER_H
 
+#include <QString>
+
+class QSqlDatabase;
+const QString DATABASE_FILENAME = "gallery.db";
+
 class DatabaseManager
 {
 public:
-        DatabaseManager() = default;
+        static DatabaseManager& instance();
+
+        virtual ~DatabaseManager();
+        DatabaseManager(const DatabaseManager&) = delete;
+        DatabaseManager(DatabaseManager&&)      = delete;
+        DatabaseManager& operator=(DatabaseManager&&) = delete;
+
+protected:
+        DatabaseManager(const QString& path = DATABASE_FILENAME);
+        DatabaseManager& operator=(const DatabaseManager& rhs);
+
+private:
+        QSqlDatabase* m_dataBase;
 };
 
 #endif  // DATABASEMANAGER_H
