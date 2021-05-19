61c9a37e85e330b6ce16bb2dad212b1167384e7d
Piyush Kumar <piyushkumg@gmail.com>
2021-05-19 21:16:41 +0530

    Add overrides in AlbumModel

--- gallery-core/include/AlbumModel.h
+++ gallery-core/include/AlbumModel.h
@@ -2,6 +2,7 @@
 #define ALBUMMODEL_H
 
 #include <QAbstractListModel>
+#include <QHash>
 
 #include "gallery-core_global.h"
 
@@ -11,5 +12,12 @@ class GALLERYCORE_EXPORT AlbumModel : public QAbstractItemModel
 
 public:
         explicit AlbumModel(QObject* parent = nullptr);
+
+        int  rowCount(const QModelIndex& parent = QModelIndex()) const override;
+        auto data(const QModelIndex& index, int role = Qt::DisplayRole) const -> QVariant override;
+        auto roleNames() const -> QHash<int, QByteArray> override;
+
+        bool setData(const QModelIndex& index, const QVariant& value, int role) override;
+        bool removeRows(int row, int count, const QModelIndex& parent) override;
 };
 #endif  // ALBUMMODEL_H
