927d6d11cb30f7d9ecad743cb07e2f070991a532
Piyush Kumar <piyushkumg@gmail.com>
2021-05-20 22:19:06 +0530

    Implement Album methods

--- gallery-core/src/Album.cpp
+++ gallery-core/src/Album.cpp
@@ -7,3 +7,23 @@ Album::Album(QString name) :
     m_name{std::move(name)}
 {
 }
+
+int Album::id() const
+{
+        return m_id;
+}
+
+void Album::setId(int id)
+{
+        m_id = id;
+}
+
+auto Album::name() const -> QString
+{
+        return m_name;
+}
+
+void Album::setName(const QString& name)
+{
+        m_name = name;
+}
