ce24a7abd70ea40fae63f1d79e524ada1c2d85f0
Piyush Kumar <piyushkumg@gmail.com>
2021-05-20 18:58:53 +0530

    Fully implement PictureModel  - Very similar to AlbumModel  - Constructor is only used to connect  - Loading pictures can only be done once we know the id  - If m_albumId is negative, clear all the pictures  - Use the = operator overload for std::unique_ptr to trasnfer ownership in loadPictures()

--- gallery-core/include/PictureModel.h
+++ gallery-core/include/PictureModel.h
@@ -1,15 +1,49 @@
 #ifndef PICTUREMODEL_H
 #define PICTUREMODEL_H
 
+#include <memory>
+#include <vector>
+
 #include <QAbstractListModel>
 
+#include "AlbumModel.h"
+#include "DatabaseManager.h"
+#include "Picture.h"
 #include "gallery-core_global.h"
 
 class GALLERYCORE_EXPORT PictureModel : public QAbstractItemModel
 {
         Q_OBJECT
 public:
-        explicit PictureModel(QObject* parent = nullptr);
+        explicit PictureModel(const AlbumModel& albumModel, QObject* parent = nullptr);
+
+        enum Roles
+        {
+                urlRole = Qt::UserRole + 1,
+                filePathRole
+        };
+
+        auto addPicture(const Picture& picture) -> QModelIndex;
+
+        int  rowCount(const QModelIndex& parent = QModelIndex()) const override;
+        auto data(const QModelIndex& index, int role) const -> QVariant override;
+        auto roleNames() const -> QHash<int, QByteArray> override;
+
+        bool removeRows(int row, int count, const QModelIndex& parent) override;
+
+        void setAlbumId(int albumId);
+        void clearAlbum();
+
+public slots:
+        void deletePicturesForAlbum();
+
+private:
+        DatabaseManager&                                       m_db;
+        int                                                    m_albumId;
+        std::unique_ptr<std::vector<std::unique_ptr<Picture>>> m_pictures;
+
+        void loadPictures(int albumId);
+        bool isIndexValid(const QModelIndex& index) const;
 };
 
 #endif  // PICTUREMODEL_H
--- gallery-core/src/PictureModel.cpp
+++ gallery-core/src/PictureModel.cpp
@@ -1,1 +1,125 @@
 #include "include/PictureModel.h"
+
+PictureModel::PictureModel(const AlbumModel& albumModel, QObject* parent) :
+    QAbstractItemModel(parent),
+    m_db(DatabaseManager::instance()),
+    m_albumId(-1),
+    m_pictures(new std::vector<std::unique_ptr<Picture>>())
+{
+        connect(&albumModel, &AlbumModel::rowsRemoved,
+                this, &PictureModel::deletePicturesForAlbum);
+}
+
+auto PictureModel::addPicture(const Picture& picture) -> QModelIndex
+{
+        int rows = rowCount();
+
+        beginInsertRows(QModelIndex(), rows, rows);
+        auto newPicture = std::make_unique<Picture>(picture);
+        m_db.pictureDao.addPictureInAlbum(m_albumId, *newPicture);
+        m_pictures->push_back(move(newPicture));
+        endInsertRows();
+
+        return index(rows, 0);
+}
+
+int PictureModel::rowCount(const QModelIndex& parent) const
+{
+        Q_UNUSED(parent);
+        return m_pictures->size();
+}
+
+auto PictureModel::data(const QModelIndex& index, int role) const -> QVariant
+{
+        if (not isIndexValid(index))
+        {
+                return QVariant();
+        }
+
+        const Picture& picture = *(m_pictures->at(index.row()));
+        switch (role)
+        {
+                case Qt::DisplayRole:
+                        return picture.fileUrl().fileName();
+
+                case Roles::urlRole:
+                        return picture.fileUrl();
+
+                case Roles::filePathRole:
+                        return picture.fileUrl().toLocalFile();
+
+                default:
+                        return QVariant();
+        }
+}
+
+auto PictureModel::roleNames() const -> QHash<int, QByteArray>
+{
+        QHash<int, QByteArray> roles;
+
+        roles[Qt::DisplayRole]     = "name";
+        roles[Roles::filePathRole] = "filepath";
+        roles[Roles::urlRole]      = "url";
+
+        return roles;
+}
+
+bool PictureModel::removeRows(int row, int count, const QModelIndex& parent)
+{
+        if (row < 0
+            || row >= rowCount()
+            || count < 0
+            || (row + count) > rowCount())
+        {
+                return false;
+        }
+
+        beginRemoveRows(parent, row, row + count - 1);
+        int countLeft = count;
+        while (countLeft--)
+        {
+                const Picture& picture = *(m_pictures->at(row + countLeft));
+                m_db.pictureDao.removePicture(picture.id());
+        }
+        m_pictures->erase(m_pictures->begin() + row,
+                          m_pictures->begin() + row + count);
+        endRemoveRows();
+
+        return true;
+}
+
+void PictureModel::setAlbumId(int albumId)
+{
+        beginResetModel();
+        m_albumId = albumId;
+        loadPictures(m_albumId);
+        endResetModel();
+}
+
+void PictureModel::clearAlbum()
+{
+        setAlbumId(-1);
+}
+
+void PictureModel::deletePicturesForAlbum()
+{
+        m_db.pictureDao.removePicturesForAlbum(m_albumId);
+        clearAlbum();
+}
+
+void PictureModel::loadPictures(int albumId)
+{
+        if (albumId <= 0)
+        {
+                m_pictures = std::make_unique<std::vector<std::unique_ptr<Picture>>>();
+                return;
+        }
+        m_pictures = m_db.pictureDao.picturesForAlbum(albumId);
+}
+
+bool PictureModel::isIndexValid(const QModelIndex& index) const
+{
+        return not(index.row() < 0
+                   || index.row() >= rowCount()
+                   || not index.isValid());
+}
